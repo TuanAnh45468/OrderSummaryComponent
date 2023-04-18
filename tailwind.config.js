506c108a27ebbecc14d7c7fe655b4e5432f11b9d
tuana <tuananh45468@gmail.com>
2023-04-18 11:09:56 +0700

    complete mobile screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,33 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    colors:{
-      "pale-blue": "hsl(225, 100%, 94%)",
+    colors: {
+      "pale-blue": "hsl(225, 100%, 60%)",
       "bright-blue": "hsl(245, 75%, 52%)",
       "very-pale-blue": "hsl(225, 100%, 98%)",
       "desaturated-blue": "hsl(224, 23%, 55%)",
       "dark-blue": "hsl(223, 47%, 23%)",
-      ...colors
+      ...colors,
     },
     extend: {
       fontFamily: {
-        "Red-Hat-Display": ['Red Hat Display', 'sans-serif']
+        "Red-Hat-Display": ["Red Hat Display", "sans-serif"],
       },
       fontWeight: {
-        "normal": 500,
-        "bold" : 700,
-        "extra-bold": 900
-      }
+        normal: 500,
+        bold: 700,
+        "extra-bold": 900,
+      },
+      backgroundImage: {
+        "desktop-background": "url('images/pattern-background-desktop.svg')",
+        "mobile-background": "url('images/pattern-background-mobile.svg')",
+      },
+      backgroundSize: {
+        "50%": "50%",
+      },
     },
   },
   plugins: [],
-}
-
+};
